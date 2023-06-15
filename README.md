7220dd90bd8c5fdb09b60c7366c4665c4943e0f7
Haricharan1212 <haricharan12122003@gmail.com>
2023-06-15 22:23:15 +0530

    Added Anti-aliasing, hittable_objects_list

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ray-tracer
+# Ray Tracer
+
+This is a simple and efficient ray-tracer I developed in C++.
+
+You can use this by adding objects to the `hittable_list` object and watch them get reflected!
--- source/main1.cpp
+++ source/main1.cpp
@@ -11,18 +11,23 @@
 
 using namespace std;
 
-color ray_color(const ray &r, const hittable &world)
+color ray_color(const ray &r, const hittable &world, int depth)
 {
     hit_record rec;
+
+    if (depth <= 0)
+        return color(0, 0, 0);
+
     if (world.hit(r, 0, infinity, rec))
     {
-        return 0.5 * (rec.normal + color(1, 1, 1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
+
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
-
 int main()
 {
     ofstream ImageFile("image.ppm");
@@ -30,11 +35,12 @@ int main()
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 50;
+    const int max_depth = 50;
 
     hittable_list world;
     world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(make_shared<sphere>(point3(1, 0, -1), 0.5));
+    // world.add(make_shared<sphere>(point3(1, 0, -1), 0.5));
 
     // Camera
 
@@ -53,11 +59,10 @@ int main()
 
             for (int s = 0; s < samples_per_pixel; ++s)
             {
-
                 auto u = double(i) / (image_width - 1);
                 auto v = double(j) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
 
             write_color(ImageFile, pixel_color, samples_per_pixel);
